940275cbf46127dd30805637da6254d6d7277cfd
Darlan <darlaneduardoguimaraes@gmail.com>
2023-06-29 16:01:25 -0300

    fix: routes and pages

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../style/global.css';
+const HomePage: React.FC = () => {
+    return <h1>Home Page</h1>;
+};
+
+export default HomePage;
---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from "../pages/home";
+const AppRouter: React.FC = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRouter;
