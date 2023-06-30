792266c242f059d50a9846883d0ecec7b5303f28
Darlan <darlaneduardoguimaraes@gmail.com>
2023-06-30 11:25:59 -0300

    fix: struct cardapio page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,27 +1,50 @@
-import React, { useState, useEffect } from 'react';
-
+import React, {useState, useEffect, useRef} from 'react';
 import * as style from './style';
 
+import user from '../../assets/elipse.png';
+import row from '../../assets/row.png'
+import file from '../../assets/file-text.png';
+import order from '../../assets/order.png';
+import input from '../../assets/input.png';
+import search from '../../assets/search.png';
 const HomePage: React.FC = () => {
-    const [pageStatus, setPageStatus] = useState<
-        'loading' | 'error' | 'success' | 'noResult'
-    >('loading');
-
-    const callApi = async () => {
-        try {
-            setPageStatus('loading');
-        } catch (error){
-            setPageStatus('error');
-        } finally {
-            setPageStatus('success');
-        }
-    }
-
     return (
         <style.Container>
             <style.Header>
-                <h1>test</h1>
+                <div className="user">
+                    <img src={user} alt="Usuário" />
+                    <div className="row">
+                        <img src={row} alt="Flexa de Opções" />
+                    </div>
+                </div>
+                <div className="file">
+                    <img src={file} alt="Cardapio"/>
+                </div>
+                <div className="order">
+                    <img src={order} alt="Pedidos"/>
+                </div>
             </style.Header>
+            <style.Main>
+                <div className="nav">
+                    <h1>Cardápio</h1>
+                    <div className="search">
+                            <label htmlFor="search">
+                                <div className="input">
+                                        <img src={search} alt={"Pesquisar"}/>
+                                        <input
+                                            id="search"
+                                            type="search"
+                                            placeholder="Buscar ..."
+                                        />
+                                </div>
+                            </label>
+                        <img src={input} alt="Adicionar"/>
+                    </div>
+                </div>
+                <div className="content">
+
+                </div>
+            </style.Main>
         </style.Container>
     );
 };
--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -5,10 +5,135 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
   min-height: 100vh;
+  
+  display: flex;
 `;
 
 export const Header = styled.header`
+  position: sticky;
   width: 112px;
   height: 100vh;
-  background: ${colors.primary};
+  
+  top: 0;
+  padding-top: 24px;
+    
+  border-right: 2px solid #E7E7E7;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+    .user {
+      height: 56px;
+      width: 56px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      
+          img{
+            max-width: 48px;
+            max-height: 48px;
+            position: absolute;
+          }
+          
+          .row{
+            cursor: pointer;
+            img{
+              max-width: 26px;
+              max-height: 26px;
+            }
+          }
+      }
+  
+      .file{
+        margin-top: 56px;
+        cursor: pointer;
+        img{
+          max-width: 32px;
+          max-height: 32px;
+        }
+      }
+  
+      .order{
+        margin-top: 24px;
+        cursor: pointer;
+        img{
+          max-width: 48px;
+          max-height: 48px;
+        }
+      }
 `;
+
+export const Main = styled.div`
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  
+  .nav{
+    position: sticky; 
+    
+    width: 100%;
+    height: 96px;
+    
+    padding: 32px 48px;
+    
+    display: flex;
+    justify-content: space-between;
+    
+    border-bottom: 2px solid #E7E7E7;
+    
+    .search{
+      display: flex;
+      justify-content: space-between;
+      
+      height: 48px;
+      width: 438px;
+      
+      img{
+        cursor: pointer;
+        max-width: 48px;
+        max-height: 48px;
+        border-radius: 8px;
+      }
+      
+      .input{
+        width: 350px;
+        height: 48px;
+        padding: 12px 16px;
+        
+        border: 2px solid #E7E7E7;
+        border-radius: 8px;
+        
+        display: flex;
+        align-items: center;
+        img{
+          max-width: 20px;
+          max-height: 20px;
+        }
+        
+        input{
+          border: none;
+          background-color: transparent;
+          height: 46px;
+          width: 302px;
+          padding: 12px;
+        }
+      }
+    }
+  }
+  
+  .content{
+    height: 100%;
+    width: 100%;
+
+    flex-grow: 1;
+  }
+`;
+
+
+
+
